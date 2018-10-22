d8c6aa51f35d965c3da409ee9deca75070acbd78
DIPTARK BOSE <diptark.bose@gmail.com>
2018-10-22 23:56:35 +0530

    Added Request Pane

---
+++ RequestAsset.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="RequestAsset.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    
+    Available Resources: <br/><br />
+    <asp:GridView ID="GridView1" runat="server" DataSourceID="SqlDataSource1" >
+    </asp:GridView>
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:AssetManagerConnectionString %>" SelectCommand="SELECT * FROM [Assets]"></asp:SqlDataSource>
+    <br/><br />
+    <asp:DropDownList ID="DropDownList2" runat="server">
+    </asp:DropDownList>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="Button1" runat="server" Text="Request" OnClick="Button1_Click" />
+    
+    <br />
+    <asp:Label ID="Label2" runat="server" Text=""></asp:Label>
+    
+</asp:Content>
---
+++ RequestAsset.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        using (SqlConnection con = new SqlConnection())
+        {
+            con.ConnectionString = @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=AssetManager;Integrated Security=True;Pooling=False";
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.Connection = con;
+                cmd.CommandText = "SELECT * FROM Assets";
+                con.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        ListItem newItem = new ListItem();
+                        newItem.Text = reader["ProductName"].ToString();
+                        int total, allocated;
+                        int.TryParse(reader["Total"].ToString(), out total);
+                        int.TryParse(reader["Allocated"].ToString(), out allocated);
+                        newItem.Value = (total - allocated).ToString();
+                        DropDownList2.Items.Add(newItem);
+                    }
+                }
+            }
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        int req, available;
+        bool check = int.TryParse(TextBox1.Text, out req);
+        int.TryParse(DropDownList2.SelectedItem.Value, out available);
+        if(check)
+        {
+            if (available >= req)
+                Label2.Text = "Request Submitted Succesfully!";
+            else
+                Label2.Text = "Requested quantity exceeds available quantity. Please check the availability from the table above.";
+        }
+    }
+}
